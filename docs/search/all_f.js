38c0ca27f8149458ad12f4018aafd9bdb64e323d
Jim Carr <jfcarr@gmail.com>
2021-08-05 23:26:59 -0400

    Generated documentation.

--- docs/search/all_f.js
+++ docs/search/all_f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['w_5fto_5fdegrees_72',['w_to_degrees',['../namespacepa__macros.html#a7686d5e038d0f28383361f108f372257',1,'pa_macros']]],
-  ['warning_73',['warning',['../namespacepa__types.html#abd8e28d1f6cc273e99b3cbaf226bb55aa7b83d3f08fa392b79e3f553b585971cd',1,'pa_types']]],
-  ['warning_5fflags_74',['warning_flags',['../namespacepa__types.html#abd8e28d1f6cc273e99b3cbaf226bb55a',1,'pa_types']]]
+  ['w_5fto_5fdegrees_73',['w_to_degrees',['../namespacepa__macros.html#a7686d5e038d0f28383361f108f372257',1,'pa_macros']]],
+  ['warning_74',['warning',['../namespacepa__types.html#abd8e28d1f6cc273e99b3cbaf226bb55aa7b83d3f08fa392b79e3f553b585971cd',1,'pa_types']]],
+  ['warning_5fflags_75',['warning_flags',['../namespacepa__types.html#abd8e28d1f6cc273e99b3cbaf226bb55a',1,'pa_types']]]
 ];
